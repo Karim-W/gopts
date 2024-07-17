e9a5c2f2322246347d30701b31659518039a2211
karim-w <karim.wael@gmail.com>
2024-07-17 08:32:25 +0400

    feat: polished API and added new functions for `getOrElse` and `Get`

--- gopts.go
+++ gopts.go
@@ -2,8 +2,19 @@ package gopts
 
 // Option is a generic type that can be used to represent a value
 // that may or may not be present.
-type Option[T any] struct {
-	value *T
+type _option[T any] struct {
+	value T
+	some  bool
+}
+
+// Option is a generic interface that can be used to represent a value
+// that may or may not be present.
+type Option[T any] interface {
+	IsSome() bool
+	IsNone() bool
+	Unwrap() T
+	GetOrElse(T) T
+	Get() (T, bool)
 }
 
 // Some returns an Option[T] with a value.
@@ -12,7 +23,7 @@ type Option[T any] struct {
 //	opt := Some(42)
 //	fmt.Println(opt.IsSome()) // true
 func Some[T any](value T) Option[T] {
-	return Option[T]{&value}
+	return &_option[T]{value, true}
 }
 
 // None returns an Option[T] with no value.
@@ -21,7 +32,7 @@ func Some[T any](value T) Option[T] {
 //	opt := None()
 //	fmt.Println(opt.IsNone()) // true
 func None[T any]() Option[T] {
-	return Option[T]{nil}
+	return &_option[T]{some: false}
 }
 
 // IsSome returns true if the Options[T] has a value.
@@ -29,8 +40,8 @@ func None[T any]() Option[T] {
 //
 //	opt := Some(42)
 //	fmt.Println(opt.IsSome()) // true
-func (o *Option[T]) IsSome() bool {
-	return o.value != nil
+func (o *_option[T]) IsSome() bool {
+	return o.some
 }
 
 // IsNone returns true if the Options[T] has no value.
@@ -38,8 +49,8 @@ func (o *Option[T]) IsSome() bool {
 //
 //	opt := None()
 //	fmt.Println(opt.IsNone()) // true
-func (o *Option[T]) IsNone() bool {
-	return o.value == nil
+func (o *_option[T]) IsNone() bool {
+	return o.some == false
 }
 
 // Unwrap returns the value of the Options[T].
@@ -48,9 +59,39 @@ func (o *Option[T]) IsNone() bool {
 //
 //	opt := Some(42)
 //	fmt.Println(opt.Unwrap()) // 42
-func (o *Option[T]) Unwrap() T {
-	if o.value == nil {
-		panic("Unwrap a None value")
+func (o *_option[T]) Unwrap() (res T) {
+	if o.some == false {
+		panic("Unwrap called on None")
 	}
-	return *o.value
+
+	return o.value
+}
+
+// GetOrElse returns the value of the Options[T].
+// If the Options[T] has no value, it returns the default value.
+// Example:
+//
+//	opt := Some(42)
+//	fmt.Println(opt.GetOrElse(0)) // 42
+func (o *_option[T]) GetOrElse(defaultValue T) (res T) {
+	if o.some == false {
+		return defaultValue
+	}
+
+	return o.value
+}
+
+// Get returns the value of the Options[T] and a boolean indicating
+// if the value is present.
+// Example:
+//
+//	opt := Some(42)
+//	val, ok := opt.Get()
+//	fmt.Println(val, ok) // 42, true
+func (o *_option[T]) Get() (res T, ok bool) {
+	if o.some == false {
+		return
+	}
+
+	return o.value, true
 }
--- gopts_test.go
+++ gopts_test.go
@@ -32,3 +32,31 @@ func TestUnwrapNone(t *testing.T) {
 	opt := None[int]()
 	opt.Unwrap()
 }
+
+func TestGetOrElse(t *testing.T) {
+	opt := Some(42)
+	if opt.GetOrElse(0) != 42 {
+		t.Error("opt.GetOrElse(0) should be 42")
+	}
+}
+
+func TestGetOrElseNone(t *testing.T) {
+	opt := None[int]()
+	if opt.GetOrElse(0) != 0 {
+		t.Error("opt.GetOrElse(0) should be 0")
+	}
+}
+
+func TestGet(t *testing.T) {
+	opt := Some(42)
+	if v, ok := opt.Get(); v != 42 || !ok {
+		t.Error("opt.Get() should be 42, true")
+	}
+}
+
+func TestGetNone(t *testing.T) {
+	opt := None[int]()
+	if v, ok := opt.Get(); v != 0 || ok {
+		t.Error("opt.Get() should be 0, false")
+	}
+}
